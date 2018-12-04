10fe51ba8e55b1ef9c0fe8e35bcb214d0b73779b
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-04 14:42:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,71 @@
+// == // loosly equal
+// === // stricty equal
+// != // loosly not equal
+// !== // stricty not equal
+
+
+
+
+
+
+//eat examaple:
+
+// not going to eat in the restaurant
+// let eat;
+// console.log(eat);
+
+// let tacosAvailable = 5; // this value would come from server;
+// let tacoCost = 1;
+
+// if (tacosAvailable > 3 || tacoCost < 1.5) {
+// eat = 'tacos';
+// }
+
+// console.log(eat);
+
+
+
+
+
+
+
+
+
+
+
+// let buyTacosQuantity = 0;
+// let tacoCost = 2;
+
+// switch (tacoCost) {
+//   case 1:
+//     buyTacosQuantity = 3;
+//   case 2:
+//     buyTacosQuantity = 2;
+//   case 3:
+//     buyTacosQuantity = 1;
+//   default:
+//     buyTacosQuantity = 0;
+// }
+
+// console.log(buyTacosQuantity);
+
+
+
+
+// if (tacoCost === 1) {
+//   buyTacosQuantity = 3;
+// } else if (tacoCost === 2) {
+//   buyTacosQuantity = 2;
+// } else if (tacoCost === 2.50) {
+//   buyTacosQuantity = 1;
+// } else if (tacoCost > 3.00) {
+//   buyTacosQuantity = 0;
+// }
+
+
+
+
+
+
+
+// '🐟' < '🐠'
