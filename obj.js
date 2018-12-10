8d53f60bb88a2bff1f728e643c96a9b9abe05742
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-10 16:57:58 +0000

    Automatically backed up by Learn

---
+++ obj.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let moonPhases = {
+  full:   '🌕',
+  waning: '🌖',
+  new:    '🌑'
+};
+
+
+// const moons = {
+//   phases: {
+//     full:   '🌕',
+//     waning: '🌖',
+//     new:    '🌑',
+//     withFaces: ['🌝', '🌚']
+//   }
+// };
+
+
+// faces = '🌝', '🌚';
+// waxing 🌔
