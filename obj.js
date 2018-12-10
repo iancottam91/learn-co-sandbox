d8dca26c93013b64377ead2cd1da5a8bc94a2dae
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-10 20:20:24 +0000

    Automatically backed up by Learn

---
+++ obj.js
@@ -0,0 +1,55 @@
+const moons = {
+  full:   '🌕',
+  waning: '🌖',
+  new:    '🌑',
+  withFaces: ['🌝', '🌚'],
+  withoutFaces: ['full', 'empty']
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var food = ['🌭', '🌮', '🍔'];
+
+// {
+//   hotdog: '🌭',
+//   taco:   '🌮',
+//   burger: '🍔'
+// }
+
+// const moonPhases = {
+//   full:   '🌕',
+//   waning: '🌖',
+//   new:    '🌑'
+// };
+
+// '🌝', '🌚'
+
+
+// const moons = {
+//   fullMoon: true,
+//   phases: {
+//     full:   '🌕',
+//     waning: '🌖',
+//     new:    '🌑',
+//     withFaces: ['🌝', '🌚']
+//   }
+// };
+
